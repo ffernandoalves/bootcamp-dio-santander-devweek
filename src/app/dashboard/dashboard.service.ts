dc3af7eca26d50538daa46eaab189a259de774de
ffernandoalves <fernandoribeiro889@gmail.com>
2021-05-29 23:06:48 -0300

    correções: baseUrl, index.html, justify-content

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'http://bootcamp-dio-santander-devweek.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://bootcamp-dio-santander-devweek.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) { }
 
